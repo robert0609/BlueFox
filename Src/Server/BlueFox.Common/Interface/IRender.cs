281794a89415fb6587069d95c1beb7e2ce6c0fd4
lanhu <robert0609@163.com>
2013-04-05 11:04:16 +0800

    modify

--- Src/Server/BlueFox.Common/Interface/IRender.cs
+++ Src/Server/BlueFox.Common/Interface/IRender.cs
@@ -7,5 +7,34 @@ namespace BlueFox.Common.Interface
 {
     public interface IRender
     {
+        Location CLocation
+        {
+            get;
+            set;
+        }
+
+        Size CSize
+        {
+            get;
+            set;
+        }
+
+        Location SLocation
+        {
+            get;
+            set;
+        }
+
+        Size SSize
+        {
+            get;
+            set;
+        }
+
+        int ZOrder
+        {
+            get;
+            set;
+        }
     }
 }
